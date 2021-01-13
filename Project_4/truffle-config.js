db1c50f392470b1045cb4951160938576c425425
Saugata Ghosh <saugatoghosh@gmail.com>
2021-01-13 20:12:16 +0530

    project 4 commit 3

--- Project_4/truffle-config.js
+++ Project_4/truffle-config.js
@@ -1,28 +1,32 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "scene paper column toward number run merit latin donkey extend photo museum";
+var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
 
 
 module.exports = {
   networks: {
     
-    /*
+    
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
       gas: 6721975
-    }
-    */
-    
-    
-    
-    development: {
+    },
+  
+  
+    development2: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        var wallet = new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        var nonceTracker = new NonceTrackerSubprovider()
+        wallet.engine._providers.unshift(nonceTracker)
+        nonceTracker.setEngine(wallet.engine)
+        return wallet
       },
       network_id: '*',
       gas: 6721975
     }
+    
     
     
   },
